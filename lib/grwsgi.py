f7737d87050fe051c805198b87893498c5002b1b
Orlando Vazquez <ovazquez@gmail.com>
2008-12-27 20:30:04 -0800

    do the simplest thing to get a list of test repos

--- lib/grwsgi.py
+++ lib/grwsgi.py
@@ -1,15 +1,19 @@
 #!python
 
-import os, sys
+import os
+import sys
+import re
 sys.path.append('/home/orlando/projects/gr.git/lib')
 
 from gitrest import GitRest
 
 repos = {
-    'sample.git': '/home/orlando/projects/sample.git',
-    'gr.git': '/home/orlando/projects/gr.git',
-    'sudobangbang.git': '/home/orlando/projects/sudobangbang.git'        
 }
+
+for dir in os.listdir('repos'):
+    match = re.match('([^/]+)\\.git$', dir)
+    if match:
+        repos[match.groups()[0]] = dir
 
 def application(environ, start_response):
     gr = GitRest(environ, start_response)
