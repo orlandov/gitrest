82796b976c16ec7a491ccb0a4c66cd8ade8b3462
Orlando Vazquez <ovazquez@gmail.com>
2008-12-27 16:10:15 -0800

    Launch own http server instead of apache for tests. This is exciting because it means that this git repo is fully self-contained, ie doesn't require any additional software to be installed (aside from some 3rd party modules).

--- tests/test_repos.py
+++ tests/test_repos.py
@@ -2,11 +2,17 @@
 
 import unittest
 from httplib import HTTPConnection
+import random
+import threading
+
+import grwsgi
+from wsgiref.simple_server import make_server
 
 class TestRepos(unittest.TestCase):
     def setUp(self):
         self.server = 'localhost'
-        self.base_path = '/gr'
+        self.base_path = ''
+        self.start_server()
 
     def test_repo(self):
         self.http_get('/repos')
@@ -17,8 +23,10 @@ class TestRepos(unittest.TestCase):
         self.assert_code(404)
 
     def http_get(self, path):
-        c = HTTPConnection(self.server)
-        c.request('GET', self.make_rest_url(path))
+        c = HTTPConnection(self.server, self.port)
+        url = self.make_rest_url(path)
+        print url
+        c.request('GET', url)
         self._response = c.getresponse()
         print self._response.read()
 
@@ -26,4 +34,13 @@ class TestRepos(unittest.TestCase):
         self.assertEqual(self._response.status, exp_code)
         
     def make_rest_url(self, path):
-        return "%s%s" % (self.base_path, path)
+        return "%s%s" % (
+            self.base_path,
+            path
+        )
+
+    def start_server(self):
+        self.port = random.randint(1024,65000)
+        self.httpd = make_server(self.server, self.port, grwsgi.application)
+        self.thread = threading.Thread(target=self.httpd.serve_forever)
+        self.thread.start()
