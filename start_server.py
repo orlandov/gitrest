c82e498ac15c2fd685952f28eb03d828f176d640
Orlando Vazquez <ovazquez@gmail.com>
2008-12-28 22:13:34 -0800

    force a reload of the test code for every reuqest in the test standalone server

--- start_server.py
+++ start_server.py
@@ -3,8 +3,14 @@ import sys
 from wsgiref.simple_server import make_server
 
 sys.path.append('lib')
+
+import gitrest
 import grwsgi
 
+def reload_application(*args):
+    reload(gitrest)
+    return grwsgi.application(*args)
+
 port = int(sys.argv[1])
-httpd = make_server('', port, grwsgi.application)
+httpd = make_server('', port, reload_application)
 httpd.serve_forever()
