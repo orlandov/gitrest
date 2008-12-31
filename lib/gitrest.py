2578fdefb65d5acd6df09c42841258c5be13309c
Orlando Vazquez <ovazquez@gmail.com>
2008-12-31 10:47:54 -0800

    refactor controllers to use resource based system

--- lib/gitrest.py
+++ lib/gitrest.py
@@ -1,8 +1,9 @@
 #!python
 
-from routes import Mapper, request_config
+from routes import Mapper, request_config, url_for
 from cStringIO import StringIO
 from simplejson import dumps, loads
+from cgi import parse_qs
 
 import git
 
@@ -29,8 +30,11 @@ class Rest(object):
         if not self.match:
             self.status('404 Not found')
             self.write("Invalid Command")
-        else:
-            self.write("%s" % (self.match,))
+            self._make_response_headers()
+            self._start_response(self._status, self._headers)
+            return self.body()
+
+        self.run_controller()
 
         self._make_response_headers()
         self._start_response(self._status, self._headers)
@@ -55,12 +59,23 @@ class Rest(object):
 
 class GitRest(Rest):
     def init_controllers(self):
+        self.controller_map = {
+            'commits': CommitsController,
+            'repos': ReposController,
+            'branchs': BranchesController,
+            'root': RootController,
+        }
+
         self._mapper.resource('repo', 'repos')
-        self._mapper.resource('branch', 'branches', path_prefix='/repos/:repo_id', name_prefix='branch_')
+        self._mapper.resource('branch', 'branches',
+            path_prefix='/repos/:repo_id', name_prefix='branch_')
 
         # both branches and repos have commits
-        self._mapper.resource('commit', 'commits', path_prefix='/repos/:repo_id', name_prefix='commit_')
-        self._mapper.resource('commit', 'commits', path_prefix='/repos/:repo_id/branches/:branch_id', name_prefix='commit_')
+        self._mapper.resource('commit', 'commits',
+            path_prefix='/repos/:repo_id', name_prefix='commit_')
+        self._mapper.resource('commit', 'commits',
+            path_prefix='/repos/:repo_id/branches/:branch_id',
+            name_prefix='commit_')
 
         self.config = request_config()
         self.config.mapper = self._mapper
@@ -70,7 +85,7 @@ class GitRest(Rest):
         controller = self.config.mapper_dict['controller']
         ControllerClass = self.controller_map[controller]
         controller = ControllerClass(self)
-        controller.GET()
+        controller.run()
 
     def set_repos(self, repos):
         self._repos = repos
@@ -80,105 +95,138 @@ class Controller(object):
     def __init__(self, rest):
         self.rest = rest
 
-    def GET(self):
-         accept = self.rest._environ.get('HTTP_ACCEPT', 'text/html').split(',')
-         if 'text/html' in accept:
-             self.rest.content_type('text/html')
-             self.GET_html()
-         elif 'application/json' in accept:
-             self.rest.content_type('application/json')
-             self.GET_json()
-         elif 'text/plain' in accept:
-             self.rest.content_type('text/plain')
-             self.GET_plain()
-         else:
-             self.rest.content_type('text/html')
-             self.GET_html()
+    def run(self):
+        self.set_content_type()
+        action = self.rest.match['action']
+        if action not in ['show', 'index']:
+            self.rest.status('420 Invalid action')
+            self.rest.write("Invalid action")
 
-    def GET_json(self):
-        try:
-            self.rest.write(dumps(self.get_resource()))
-        except:
-            pass
+        call = getattr(self, action)
+        call()
 
-    def GET_html(self):
-        try:
-            self.rest.write("<br />".join(self.get_resource()))
-        except:
-            pass
+    def set_content_type(self):
+        self.accepts = []
+        self.accepts.extend(
+            parse_qs(self.rest._environ['QUERY_STRING'])
+            .get('accept', [''])[0]
+            .split(',')
+        )
+        self.accepts.extend(
+            self.rest._environ.get('HTTP_ACCEPT', 'text/html').split(',')
+        )
+        self.accept = self.accepts[0] if self.accepts else 'text/html'
 
-    def GET_plain(self):
-        try:
-            self.rest.write("plain %s" % (self.get_resource()))
-        except:
-            pass
+        for mimetype in ['application/json', 'text/plain', 'text/html']:
+            if mimetype == self.accept:
+                self.rest.content_type(mimetype)
+                break
 
+    def show(self):
+        if 'application/json' == self.accept:
+            self.json_resource()
+        elif 'text/plain' == self.accept:
+            self.plain_resource()
+        else:
+            self.html_resource()
+
+    def index(self):
+        if 'text/html' in self.accept:
+            self.html_collection()
+        elif 'application/json' in self.accept:
+            self.json_collection()
+        elif 'text/plain' in self.accept:
+            self.plain_collection()
+        else:
+            self.html_collection()
+
+    def json_collection(self):
+        self.rest.write(dumps(self.get_collection()))
+
+    def html_collection(self):
+        self.rest.write("<html><head><title></title></head><body>")
+        self.rest.write("<br />".join([ self.member_link(c) for c in self.get_collection()]))
+        self.rest.write("</body></html>")
+
+    def plain_collection(self):
+        self.rest.write("plain %s" % (self.get_collection()))
+
+    def json_resource(self):
+        self.rest.write(dumps(self.get_member(self.match['id'])))
+
+    def html_resource(self):
+        id = self.rest.match['id']
+        self.rest.write("<html><head><title>Repository %s</title></head><body>" % (id,))
+        self.rest.write("<table>")
+        for k,v in self.get_member(id).items():
+            self.rest.write("<tr><td>%s</td><td>%s</td></tr>" % (k, v));
+        self.rest.write("</table>")
+        self.rest.write("</body></html>")
+
+    def plain_resource(self):
+        self.rest.write("plain %s" % (self.get_collection()))
 
 class ReposController(Controller):
-    def get_resource(self):
+    def member_link(self, collection):
+        return "<a href='%s'>%s</a>" % (url_for(controller='repos', action='show', id=collection['id']), collection['id'])
+
+    def get_collection(self):
         repos = self.rest._repos.keys()
         repos.sort()
-        return repos
+        return [ self.get_member(repo_id) for repo_id in repos ]
+
+    def get_member(self, repo_id):
+        if repo_id not in self.rest._repos.keys():
+            # XXX how should we propagate errors?
+            self.rest.status('404 Repo not found')
+            return { 'id': 'not found', 'description': 'doesnt exist' }
+
+        repo = git.Repo('repos/%s.git' % (repo_id,))
+        repo_dict = {
+            'id': repo_id,
+            'description': repo.description,
+            'branches': [ b.name for b in repo.branches ],
+            'tree': [ item[0] for item in repo.tree().items() ]
+        } 
+        return repo_dict
 
 
 class RootController(Controller):
-    def get_resource(self):
-        repos = self.rest._repos.keys()
-        repos.sort()
-        return repos
-
+     def index(self):
+         self.rest.write("awesome rest server index")
+# 
+#     def show(self):
+#         #self.rest.write("awesome rest server show")
+#         pass
+#    pass
 
 class LoadsRepoMixin(object):
     def load_repo(self):
-        if not hasattr(self, 'repo'):
-            self.repo_name = self.rest.match['repo']
-            self.repo = git.Repo('repos/' + self.rest.match['repo'] + '.git')
+        if hasattr(Self, 'repo'): return
+        self.repo_name = self.rest.match['repo_id']
+        self.repo = git.Repo('repos/' + self.repo_name + '.git')
+
+    def validate_repo(self):
+        load_repo()
 
 
-class RepoController(Controller, LoadsRepoMixin):
-    def get_resource(self):
-        self.load_repo()
+class CommitsController(Controller, LoadsRepoMixin):
+    def validate(self):
+        validate_repo()
 
-        return {
-            'repo': self.repo_name,
-            'description': self.repo.description,
-            'branches': [ branch.name for branch in self.repo.branches ],
-            'tree': [ item[0] for item in self.repo.tree().items() ]
-        }
+    def member_link(self, collection):
+        return "<a href='%s'>%s</a>" % (url_for(controller='commits', repo_id=self.rest.match['repo_id'], action='show', id=collection['id']), collection['id'])
 
-    @staticmethod
-    def as_html(resource):
-        repo = resource['repo']
-        desc = resource['description']
-        branches = resource['branches']
-        tree = resource['tree']
-        return "<h1>%s</h1>Description: %s<br />Branches: %s<h2>Tree:</h2>%s" % (repo, desc, ", ".join(branches), "<br />\n".join(tree))
-
-    def GET_html(self):
-        try:
-            self.rest.write(RepoController.as_html(self.get_resource()))
-        except git.NoSuchPathError:
-            self.rest.status('404 Repo not found')
-            self.rest.write("No such repo")
-
-    def GET_plain(self):
-        self.rest.write('weak')
-
-class RepoCommitsController(Controller, LoadsRepoMixin):
-    def get_resource(self):
-        self.load_repo()
+    def get_collection(self):
         return [
-            {'id': c.id, 'summary': c.summary }
+            { 'id': c.id, 'summary': c.summary }
             for c in self.repo.commits()
         ]
 
-    @staticmethod
-    def as_html(resource):
-        return "commit %s" % (str(resource),)
+    def get_member(self, id):
+        self.load_repo()
+        c = self.repo.commit(id)
+        return { 'message': c.message, 'actor': c.author.name}
 
-    def GET_html(self):
-        try:
-            self.rest.write(RepoCommitsController.as_html(self.get_resource()))
-        except git.NoSuchPathError:
-            self.rest.status('404 Repo not found')
-            self.rest.write("No such repo")
+class BranchesController(Controller, LoadsRepoMixin):
+    pass
