41518706e3ffa49bc2b7c86063b6ba8ad743a9ca
Orlando Vazquez <ovazquez@gmail.com>
2009-01-01 16:41:46 -0800

    add a binutils setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='Gitrest',
+    version='1.0',
+    description='RESTful git interface',
+    author_email='ovazquez@gmail.com',
+    author='Orlando Vazquez',
+    url='http://www.github.com/orlandov/gitrest/'
+)
