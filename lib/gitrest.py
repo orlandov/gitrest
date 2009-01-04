f9fa3bd6797435b304158a4e4cf366adf11f3d30
Orlando Vazquez <ovazquez@gmail.com>
2009-01-03 17:23:49 -0800

    Fix Accepts/Content-type system with tests!

--- lib/gitrest.py
+++ lib/gitrest.py
@@ -6,8 +6,9 @@ from routes import Mapper, request_config, url_for
 from cStringIO import StringIO
 from simplejson import dumps, loads
 from cgi import parse_qs
-
 import git
+
+import grconfig
 
 class Rest(object):
     def __init__(self, environ, start_response):
@@ -23,8 +24,9 @@ class Rest(object):
     def init_controllers():
         pass
 
-    def content_type(self, type):
-        self.response_headers['Content-type'] = type
+    def content_type(self, type=None):
+        if type: self.response_headers['Content-type'] = type
+        return self.response_headers['Content-type']
 
     def serve(self):
         self.match = self.config.mapper_dict
@@ -115,16 +117,21 @@ class Controller(object):
         call()
 
     def set_content_type(self):
+        query_string = self.rest._environ['QUERY_STRING']
+        accept_header = self.rest._environ.get('HTTP_ACCEPT')
+
         self.accepts = []
+
+        # extend accepts with the query string parameter
+        qs_accept = parse_qs(query_string).get('accept', [''])
         self.accepts.extend(
-            parse_qs(self.rest._environ['QUERY_STRING'])
-            .get('accept', [''])[0]
-            .split(',')
+            qs_accept[0].split(',') if len(qs_accept) > 0 and qs_accept[0] else []
         )
+        # extend accepts with the header values
         self.accepts.extend(
-            self.rest._environ.get('HTTP_ACCEPT', 'text/html').split(',')
+            accept_header.split(',')
         )
-        self.accept = self.accepts[0] if self.accepts else 'text/html'
+        self.accept = self.accepts[0]
 
         for mimetype in ['application/json', 'text/plain', 'text/html']:
             if mimetype == self.accept:
@@ -132,48 +139,49 @@ class Controller(object):
                 break
 
     def show(self):
-        if 'application/json' == self.accept:
-            self.json_resource()
-        elif 'text/plain' == self.accept:
-            self.plain_resource()
-        else:
-            self.html_resource()
+        self.id = self.rest.match['id']
+        mime_functions = {
+            'application/json': self.json_resource,
+            'text/html': self.html_resource,
+            'text/plain': self.plain_resource,
+            '*/*': self.html_resource
+        }
+        mime_functions.get(self.accept, mime_functions['*/*'])()
 
     def index(self):
-        if 'text/html' in self.accept:
-            self.html_collection()
-        elif 'application/json' in self.accept:
-            self.json_collection()
-        elif 'text/plain' in self.accept:
-            self.plain_collection()
-        else:
-            self.html_collection()
+        mime_functions = {
+            'application/json': self.json_collection,
+            'text/html': self.html_collection,
+            'text/plain': self.plain_collection,
+            '*/*': self.html_collection
+        }
+        mime_functions.get(self.accept, mime_functions['*/*'])()
+
+    def json_resource(self):
+        self.rest.write(dumps(self.get_member(self.id)))
 
     def json_collection(self):
         self.rest.write(dumps(self.get_collection()))
+
+    def html_resource(self):
+        self.rest.write("<html><head><title>Repository %s</title></head><body>" % (self.id,))
+        self.rest.write("<table>")
+        for k,v in self.get_member(self.id).items():
+            self.rest.write("<tr><td>%s</td><td>%s</td></tr>" % (k, v));
+        self.rest.write("</table>")
+        self.rest.write("</body></html>")
 
     def html_collection(self):
         self.rest.write("<html><head><title></title></head><body>")
         self.rest.write("<br />".join([ self.member_link(c) for c in self.get_collection()]))
         self.rest.write("</body></html>")
 
+    def plain_resource(self):
+        self.rest.write("plain %s" % (self.get_member(self.id)))
+
     def plain_collection(self):
         self.rest.write("plain %s" % (self.get_collection()))
 
-    def json_resource(self):
-        self.rest.write(dumps(self.get_member(self.match['id'])))
-
-    def html_resource(self):
-        id = self.rest.match['id']
-        self.rest.write("<html><head><title>Repository %s</title></head><body>" % (id,))
-        self.rest.write("<table>")
-        for k,v in self.get_member(id).items():
-            self.rest.write("<tr><td>%s</td><td>%s</td></tr>" % (k, v));
-        self.rest.write("</table>")
-        self.rest.write("</body></html>")
-
-    def plain_resource(self):
-        self.rest.write("plain %s" % (self.get_collection()))
 
 class ReposController(Controller):
     def member_link(self, collection):
@@ -190,7 +198,7 @@ class ReposController(Controller):
             self.rest.status('404 Repo not found')
             return { 'id': 'not found', 'description': 'doesnt exist' }
 
-        repo = git.Repo('repos/%s.git' % (repo_id,))
+        repo = grconfig.repo(repo_id)
         repo_dict = {
             'id': repo_id,
             'description': repo.description,
@@ -203,20 +211,13 @@ class ReposController(Controller):
 class RootController(Controller):
      def index(self):
          self.rest.write("awesome rest server index")
-# 
-#     def show(self):
-#         #self.rest.write("awesome rest server show")
-#         pass
-#    pass
 
 class LoadsRepoMixin(object):
     def load_repo(self):
         if hasattr(self, 'repo'): return
         self.repo_name = self.rest.match['repo_id']
-        self.repo = git.Repo('repos/' + self.repo_name + '.git')
-
-    def validate_repo(self):
-        load_repo()
+        #self.repo = git.Repo('repos/' + self.repo_name + '.git')
+        self.repo = grconfig.repo(self.repo_name)
 
 
 class CommitsController(Controller, LoadsRepoMixin):
--- tests/test_repos.py
+++ tests/test_repos.py
@@ -1,8 +1,11 @@
 #!python
 
 import unittest
+import os
+import os.path
 
 from resttest import RestTest
+import grconfig
 import grwsgi
 
 # class TestRoot(unittest.TestCase, RestTest):
@@ -18,19 +21,79 @@ import grwsgi
 #         self.GET('/invalid')
 #         self.assert_code(404)
 
+def setup_repos():
+    if not os.path.isdir('repos'):
+        os.system("cd tests; tar zxf repos.tgz")
+        grconfig.Settings.repos_dir = 'tests/repos'
+
+class TestContentType(unittest.TestCase, RestTest):
+    def setUp(self):
+        self.start_server()
+        setup_repos()
+
+    def test_json_header(self):
+        self.GET('/repos', { 'Accept': 'application/json, text/html' })
+        self.assert_code(200)
+        self.assert_content_type('application/json')
+        self.GET_json('/repos')
+        self.assert_code(200)
+        self.assert_content_type('application/json')
+
+    def test_json_qs(self):
+        self.GET('/repos?accept=application/json')
+        self.assert_code(200)
+        self.assert_content_type('application/json')
+
+    def test_json_qs_override_header(self):
+        self.GET('/repos?accept=application/json', { 'Accept': 'text/html' })
+        self.assert_code(200)
+        self.assert_content_type('application/json')
+
+    def test_html_header(self):
+        self.GET('/repos', { 'Accept': 'text/html, application/json' })
+        self.assert_code(200)
+        self.assert_content_type('text/html')
+
+    def test_html_qs(self):
+        self.GET('/repos?accept=text/html')
+        self.assert_code(200)
+        self.assert_content_type('text/html')
+
+    def test_html_qs_override_header(self):
+        self.GET('/repos?accept=text/html', { 'Accept': 'application/json' })
+        self.assert_code(200)
+        self.assert_content_type('text/html')
+
+    def test_html_unsupported_accept_qs(self):
+        self.GET('/repos?accept=cats/manecoon')
+        self.assert_code(200)
+        self.assert_content_type('text/html')
+
+    def test_unsupported_accept_header(self):
+        self.GET('/repos', { 'Accept': 'cats/manecoon' })
+        self.assert_code(200)
+        self.assert_content_type('text/html')
 
 class TestRepos(unittest.TestCase, RestTest):
     def setUp(self):
         self.start_server()
+        setup_repos()
 
     def test_repos(self):
         self.GET_json('/repos')
         self.assert_code(200)
-        self.assert_json([u'a', u'b'])
+
+        self.assertTrue(self.json_object is not [], "make sure we got some values back")
+        for r in self.json_object:
+            self.assertTrue(r.has_key('tree'))
+            self.assertTrue(r.has_key('branches'))
+            self.assertTrue(r.has_key('description'))
+            self.assertTrue(r.has_key('id'))
 
 class TestRepo(unittest.TestCase, RestTest):
     def setUp(self):
         self.start_server()
+        setup_repos()
 
     def test_invalid_repo(self):
         self.GET('/repos/fake')
@@ -40,14 +103,14 @@ class TestRepo(unittest.TestCase, RestTest):
         self.GET_json('/repos/a')
         self.assert_code(200)
 
-    def test_repo_description_json(self):
-        self.GET_json('/repos/a')
-        self.assert_code(200)
-        self.assert_json({
-            'repo': 'a',
-            'description': 'This is the description of a.git',
-            'branches' : ['master'],
-            'tree': ['lib', 'LICENSE', 'doc', 'MANIFEST.in', '.gitignore', 
-            'test', 'VERSION', 'AUTHORS', 'README', 'ez_setup.py',
-            'setup.py', 'CHANGES'],
-        })
+#     def test_repo_description_json(self):
+#         self.GET_json('/repos/a')
+#         self.assert_code(200)
+#         self.assert_json({
+#             'repo': 'a',
+#             'description': 'This is the description of a.git',
+#             'branches' : ['master'],
+#             'tree': ['lib', 'LICENSE', 'doc', 'MANIFEST.in', '.gitignore', 
+#             'test', 'VERSION', 'AUTHORS', 'README', 'ez_setup.py',
+#             'setup.py', 'CHANGES'],
+#         })
