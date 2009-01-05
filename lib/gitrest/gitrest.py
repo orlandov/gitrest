b09c1911d86b3b78a1ad621a9adccd1db19833e9
Orlando Vazquez <ovazquez@gmail.com>
2009-01-04 21:36:48 -0800

    refactor the Rest class to its own package

--- lib/gitrest/gitrest.py
+++ lib/gitrest/gitrest.py
@@ -1,7 +1,6 @@
 #!python
 
 from cgi import parse_qs
-from cStringIO import StringIO
 from routes import Mapper, request_config, url_for
 from simplejson import dumps, loads
 import git
@@ -11,58 +10,9 @@ import traceback
 import config
 import gitrest
 
-class Rest(object):
-    def __init__(self, environ, start_response):
-        self._environ = environ
-        self._start_response = start_response
-        self._status = '200 OK'
-        self.response_headers = { 'Content-type': 'text/html' }
-        self._headers = []
-        self._mapper = Mapper()
-        self._output = StringIO()
-        self.init_controllers()
+import rest.rest
 
-    def init_controllers():
-        pass
-
-    def content_type(self, type=None):
-        if type: self.response_headers['Content-type'] = type
-        return self.response_headers['Content-type']
-
-    def serve(self):
-        self.match = self.config.mapper_dict
-
-        if not self.match:
-            self.status('404 Not found')
-            self.write("Invalid Command")
-            self._make_response_headers()
-            self._start_response(self._status, self._headers)
-            return self.body()
-
-        self.run_controller()
-
-        self._make_response_headers()
-        self._start_response(self._status, self._headers)
-        return self.body()
-
-    # return the full contents of the body
-    def body(self):
-        return self._output.getvalue()
-
-    # write to the body, appends
-    def write(self, s):
-        self._output.write(s)
-
-    def status(self, s):
-        self._status = s
-
-    def _make_response_headers(self):
-        self._headers = []
-        for header in self.response_headers.keys():
-            self._headers.append((header, self.response_headers[header]))
-
-
-class GitRest(Rest):
+class GitRest(rest.rest.Rest):
     def init_controllers(self):
         self.controller_map = {
             'commits': CommitsController,
---
+++ lib/rest/rest.py
@@ -0,0 +1,51 @@
+#!python
+
+from routes import Mapper, request_config, url_for
+from cStringIO import StringIO
+
+class Rest(object):
+    def __init__(self, environ, start_response):
+        self._environ = environ
+        self._start_response = start_response
+        self._status = '200 OK'
+        self.response_headers = { 'Content-type': 'text/html' }
+        self._headers = []
+        self._mapper = Mapper()
+        self._output = StringIO()
+        self.init_controllers()
+
+    def content_type(self, type=None):
+        if type: self.response_headers['Content-type'] = type
+        return self.response_headers['Content-type']
+
+    def serve(self):
+        self.match = self.config.mapper_dict
+
+        if not self.match:
+            self.status('404 Not found')
+            self.write("Invalid Command")
+            self._make_response_headers()
+            self._start_response(self._status, self._headers)
+            return self.body()
+
+        self.run_controller()
+
+        self._make_response_headers()
+        self._start_response(self._status, self._headers)
+        return self.body()
+
+    # return the full contents of the body
+    def body(self):
+        return self._output.getvalue()
+
+    # write to the body, appends
+    def write(self, s):
+        self._output.write(s)
+
+    def status(self, s):
+        self._status = s
+
+    def _make_response_headers(self):
+        self._headers = []
+        for header in self.response_headers.keys():
+            self._headers.append((header, self.response_headers[header]))
